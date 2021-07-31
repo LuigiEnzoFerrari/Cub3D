8969e03f25c2da32c229fb4d2f934c89723f422f
LuigiFerrari <luigienzopereiraferrari@gmail.com>
2021-07-31 18:19:31 -0300

    addColorIntensity

--- srcs/render/projection.c
+++ srcs/render/projection.c
@@ -6,14 +6,28 @@
 /*   By: lenzo-pe <lenzo-pe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 11:00:29 by lenzo-pe          #+#    #+#             */
-/*   Updated: 2021/07/31 14:09:05 by lenzo-pe         ###   ########.fr       */
+/*   Updated: 2021/07/31 18:19:01 by lenzo-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 #include <stdio.h>
 
-void	texOffSet(t_rays rays, t_xPoint *offSet)
+uint32_t	colorIntesity(uint32_t color, float intensity)
+{
+	uint32_t	t;
+	uint32_t	r;
+	uint32_t	g;
+	uint32_t	b;
+
+	t = ((color & (0xff << 24)) * intensity);
+	r = ((color & (0xff << 16)) * intensity);
+	g = ((color & (0xff << 8)) * intensity);
+	b = ((color & (0xff)) * intensity);
+	return ((t & (0xff << 24)) | (r  & (0xff << 16)) | (g & (0xff << 8)) | b);
+}
+
+static void	texOffSet(t_rays rays, t_xPoint *offSet)
 {
 		if (rays.isVert)
 			offSet->x = (int)rays.wallHitY % TILE_SIZE;
@@ -21,7 +35,7 @@ void	texOffSet(t_rays rays, t_xPoint *offSet)
 			offSet->x = (int)rays.wallHitX % TILE_SIZE;
 }
 
-int	*get_texture_addr(t_ximg *tex, t_rays rays)
+static int	*get_texture_addr(t_ximg *tex, t_rays rays)
 {
 	int *dest;
 
@@ -37,21 +51,23 @@ int	*get_texture_addr(t_ximg *tex, t_rays rays)
 	return (dest);
 }
 
-void	lineWall(t_vars *vars, t_proj pro, t_xPoint res)
+static void	lineWall(t_vars *vars, t_proj pro, t_xPoint res)
 {
 	t_xPoint	offSet;
-	int			distfromtop;
-	uint32_t	colorTex;
+	int			distop;
 	int			*tex;
+	uint32_t	colorTex;
 
 	texOffSet(vars->rays[res.x], &offSet);
 	tex = get_texture_addr(vars->tex, vars->rays[res.x]);
 	res.y = pro.topPixel;
 	while (res.y < pro.bottomPixel)
 	{
-		distfromtop = res.y + (pro.stripHeight >> 1) - (vars->set.resolution.h >> 1);
-		offSet.y = distfromtop * ((double)TILE_SIZE / pro.stripHeight);
+		distop = res.y + (pro.stripHeight >> 1) - (vars->set.resolution.h >> 1);
+		offSet.y = distop * ((double)TILE_SIZE / pro.stripHeight);
 		colorTex = tex[(TILE_SIZE * offSet.y) + offSet.x];
+		if (vars->rays[res.x].isVert)
+			colorTex = colorIntesity(colorTex, 0.5);
 		mlx_set_render_color(&vars->renderer, colorTex);
 		mlx_put_pixel(&vars->renderer, res.x, res.y);
 		res.y++;
